aa643ec897e965a50547441d40357e0488844003
1siddharth <siddharth2c51@gmail.com>
2020-10-03 15:52:09 +0530

    initial commit

---
+++ auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	mongoURL :  "mongodb+srv://siddharth:siddharth@cluster0.bk7c4.gcp.mongodb.net/userinput?retryWrites=true&w=majority",
+    secret : "siddharth1"
+
+}
---
+++ client/src/components/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{color: "#6200EE"}} className="container">
+        
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">WELCOME</h1>
+            
+            <h3>THIS PAGE IS DESIGNED BY SIDDHARTH</h3>
+            
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing
